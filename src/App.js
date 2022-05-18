52e23ea39af592f05c62ee07c5d412fbcf68dbd1
liji356 <lijibthomas95@gmail.com>
2022-05-18 19:28:46 +0530

    new detalis  web

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Employedetalis from './component/Employedetalis';
 
 function App() {
   return (
     <>
     
-    <Header/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Employedetalis/>}/>
+      
+      
+    </Routes>
+    </BrowserRouter>
     
     </>
   );
